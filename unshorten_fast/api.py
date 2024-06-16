f854e0bf91b167177825e89c5c819a0316c0e66d
Giovanni Luca Ciampaglia <glciampagl@gmail.com>
2024-06-16 08:33:28 -0400

    Increase max tcp connections

--- unshorten_fast/api.py
+++ unshorten_fast/api.py
@@ -9,7 +9,7 @@ from urllib.parse import urlsplit
 import re
 
 TTL_DNS_CACHE=300  # Time-to-live of DNS cache
-MAX_TCP_CONN=50  # Throttle at max these many simultaneous connections
+MAX_TCP_CONN=200  # Throttle at max these many simultaneous connections
 TIMEOUT_TOTAL=10  # Each request times out after these many seconds
 
 LOG_FMT = "%(asctime)s:%(levelname)s:%(message)s"
